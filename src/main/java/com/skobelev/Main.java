6819f607f781c7c2b4996e858570642ad77c1e77
Grigoriy Skobelev <gskobelevff@gmail.com>
2022-04-05 20:57:47 +0300

    Fix compile error

--- src/main/java/com/skobelev/Main.java
+++ src/main/java/com/skobelev/Main.java
@@ -5,6 +5,5 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world");
         System.out.println("Ура мы подключили GitHub actions");
-        var1 := 1;
     }
 }
